5998be2ba6480cf9a809548ed8025dc6fdf44dca
Chananwat.Wan <Chananwat.Wan@LAPTOP-V54H4V6K>
2023-03-10 22:08:55 +0700

    Create new project

---
+++ ExampleBank.Web/ExampleBank.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExampleBank.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
