390af150f8e2834944a0ec6d613bd82d1d58f543
Chananwat.Wan <Chananwat.Wan@LAPTOP-V54H4V6K>
2023-03-12 17:45:54 +0700

    Get BankAccount info

--- ExampleBank.Web/ExampleBank.Web/Models/Accounts/Responses/AccountInfoResponseModel.cs
+++ ExampleBank.Web/ExampleBank.Web/Models/Accounts/Responses/AccountInfoResponseModel.cs
@@ -1,9 +1,21 @@
-﻿namespace ExampleBank.Web.Models.Accounts.Responses
+﻿using ExampleBank.Web.Enums;
+
+namespace ExampleBank.Web.Models.Accounts.Responses
 {
     public class AccountInfoResponseModel
     {
         public Guid Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public IEnumerable<BankAccountInfo> BankAccounts { get; set; }
+        public int TotalBankAccounts => BankAccounts.Count();
+
+        public class BankAccountInfo
+        {
+            public string IBAM { get; set; }
+            public string AccountId { get; set; }
+            public BankAccountType Type { get; set; }
+            public decimal Amount { get; set; }
+        }
     }
 }
--- ExampleBank.Web/ExampleBank.Web/Services/Accounts/Queries/AccountInfoQueryService.cs
+++ ExampleBank.Web/ExampleBank.Web/Services/Accounts/Queries/AccountInfoQueryService.cs
@@ -1,28 +1,40 @@
 ﻿using ExampleBank.Web.Models;
 using ExampleBank.Web.Models.Accounts.Responses;
 using ExampleBank.Web.Repositories.Accounts.Queries;
+using ExampleBank.Web.Repositories.BankAccounts.Queries;
 using Microsoft.EntityFrameworkCore;
 
 namespace ExampleBank.Web.Services.Accounts.Queries
 {
     public class AccountInfoQueryService : IAccountInfoQueryService
     {
-        public AccountInfoQueryService(IAccountQueryRepository accountQueryRepository)
+        public AccountInfoQueryService(IAccountQueryRepository accountQueryRepository
+            , IBankAccountQueryRepository bankAccountQueryRepository)
         {
             AccountQueryRepository = accountQueryRepository;
+            BankAccountQueryRepository = bankAccountQueryRepository;
         }
 
-        public IAccountQueryRepository AccountQueryRepository { get; }
+        protected IAccountQueryRepository AccountQueryRepository { get; }
+        protected IBankAccountQueryRepository BankAccountQueryRepository { get; }
 
         public async Task<ResultModel<IEnumerable<T>>> GetAsync<T>()
             where T : AccountInfoResponseModel, new()
         {
             var entities = await AccountQueryRepository.All.AsNoTracking()
+                .Include(t => t.BackAccounts)
                 .Select(s => new T
                 {
                     Id = s.Id,
                     FirstName = s.FirstName,
                     LastName = s.LastName,
+                    BankAccounts = s.BackAccounts.Select(ss => new AccountInfoResponseModel.BankAccountInfo
+                    {
+                        IBAM = ss.IBAN,
+                        AccountId = ss.AccountId.ToString(),
+                        Amount = ss.Amount,
+                        Type = ss.BankAccountType
+                    })
                 }).ToListAsync();
             return new ResultModel<IEnumerable<T>>(true, entities);
         }
